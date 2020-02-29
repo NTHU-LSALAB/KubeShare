be8e0a6a7132b6e6718789d6c23330ecc5880d72
Eric Yeh <ncy9371@hotmail.com>
2020-02-29 15:41:51 +0800

    v0.9.4 * check if CRD exist * if no require GPU, don't add annotation to Pod

--- code-gen.sh
+++ code-gen.sh
@@ -1,3 +1,4 @@
+# code-generator (branch release-1.17) commit hash: 4ae19cfe9b46bf48d232c065a9078d1dff3de06c
 GO111MODULE=off ./generate-groups.sh all \
 github.com/NTHU-LSALAB/KubeShare/pkg/client \
 github.com/NTHU-LSALAB/KubeShare/pkg/apis \
